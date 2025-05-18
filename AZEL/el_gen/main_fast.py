3013203dd4575b31f52e695886e309a3b43f769e
buranyan <9108hani@gmail.com>
2025-05-18 22:35:23 +0900

    update

--- AZEL/el_gen/main_fast.py
+++ AZEL/el_gen/main_fast.py
@@ -5,41 +5,63 @@ import matplotlib.pyplot as plt
 # =======================
 # 定数定義（グローバル定数化）
 # =======================
-azv_d = 36
-aza_d = 21.6
-ta = azv_d / aza_d
-tp = 20
-tc = tp - ta
-psi_yaw = 30
-az_center = 30 #セクタースキャン方位角の中央値
-theta_pitch = 0
-phi_roll = 20
-elev_ang = -20
+azv_d = 36 # AZ角速度[deg/s]
+aza_d = 21.6 # AZ角加速度[deg/s2]
+ta = azv_d / aza_d # AZ加速時間[s]
+tp = 20 # 連続回転時、一定角速度に換算した場合のAZ回転時間[s]
+tc = tp - ta # AZ加速時間を引き算して、一定角速度の期間を計算[s]
+psi_yaw = 0 # ヨー角[deg]
+az_center = 90 #セクタースキャン方位角の中央値[deg]
+theta_pitch = 0 # ピッチ角[deg]
+phi_roll = 20 # ロール角[deg]
+elev_ang = 0 # 仰角設定値[deg]
 
-gretio_el = 480 #100だと負荷が見えてくる。
-owntq = 2.51 / gretio_el
-JM = 6.4e-7
-DM = 1.0e-5
-JL = 0.0613 / gretio_el**2
-DL = 0.005 / gretio_el**2
-kgb = 1000
-dbdeg = 0.1
-dbrad = dbdeg * np.pi / 180
-kpp = 30
-kvp = 90
-kvi = 30
-v_lim_val = 5000 / 60 * 2 * np.pi
-v_min_val = -v_lim_val
-v_max_val = v_lim_val
-t_lim_val = 0.038  # 20Wモータ
-t_min_val = -t_lim_val
-t_max_val = t_lim_val
-fc = 10e3 # LPFのカットオフ周波数
+# EL
+gretio_el = 480 #　ELギヤ比。100だと負荷が見えてくる。
+owntq = 2.51 / gretio_el # 自重トルク[Nm]
+JM_el = 6.4e-7 # モータイナーシャ[kgm2]
+DM_el = 1.0e-5 # モータ粘性係数[Nm/(rad/s)]
+JL_el = 0.0613 / gretio_el**2 # モータ軸換算負荷イナーシャ[kgm2]
+DL_el = 0.005 / gretio_el**2 # モータ軸換算負荷粘性係数[Nm/(rad/s)]
+kgb_el = 1000 # バネ係数
+dbdeg_el = 0.1 # バックラッシ[deg]
+dbrad_el = dbdeg_el * np.pi / 180 # バックラッシ[rad]
+kpp_el = 30 # 位置比例ゲイン
+kvp_el = 90 # 速度比例ゲイン
+kvi_el = 30 # 速度積分ゲイン
+v_lim_val_el = 5000 / 60 * 2 * np.pi # 角速度リミット値[rad/s]
+v_min_val_el = -v_lim_val_el
+v_max_val_el = v_lim_val_el
+t_lim_val_el = 0.038  # トルクリミット値[Nm] 20Wモータ 0.038[Nm]
+t_min_val_el = -t_lim_val_el
+t_max_val_el = t_lim_val_el
+fc_el = 100 # LPFのカットオフ周波数[Hz]
 
-sim_time = 30 # 計算時間
-dt = 10e-6 # 10e-6 刻み値
-steps = int(sim_time / dt) # 全サンプリング数
-sample_time = 10e-3 # サンプリング期間
+# AZ
+gretio_az = 480 #　AZギヤ比。100だと負荷が見えてくる。
+JM_az = 6.4e-7 # モータイナーシャ[kgm2]
+DM_az = 1.0e-5 # モータ粘性係数[Nm/(rad/s)]
+JL_az = 10 / gretio_az**2 # モータ軸換算負荷イナーシャ[kgm2]
+DL_az = 10 / gretio_az**2 # モータ軸換算負荷粘性係数[Nm/(rad/s)]
+kgb_az = 1000 # バネ係数
+dbdeg_az = 0.1 # バックラッシ[deg]
+dbrad_az = dbdeg_az * np.pi / 180 # バックラッシ[rad]
+kpp_az = 90 # 位置比例ゲイン
+kvp_az = 9 # 速度比例ゲイン
+kvi_az = 3 # 速度積分ゲイン
+v_lim_val_az = 5000 / 60 * 2 * np.pi # 角速度リミット値[rad/s]
+v_min_val_az = -v_lim_val_az
+v_max_val_az = v_lim_val_az
+t_lim_val_az = 0.038 # トルクリミット値[Nm] 20Wモータ 0.038[Nm]
+t_min_val_az = -t_lim_val_az
+t_max_val_az = t_lim_val_az
+fc_az = 100 # LPFのカットオフ周波数[Hz]
+
+# 計算設定値
+sim_time = 30 # 計算時間[s]
+dt = 10e-6 # 10e-6 刻み値[s]
+steps = int(sim_time / dt) # 全計算数
+sample_time = 1e-3 # 1e-3 サンプリング期間[s]
 sample_n = int(sample_time / dt) # サンプリング数/期間
 
 # =======================
@@ -112,12 +134,12 @@ def az_v_s_180deg_numba(t):
     else:
         return 0
 
-# ピッチ角
+# ピッチ角設定
 @njit
 def theta_pitch_func_numba(t):
     if 0 <= t <= 1:
         return 0
-    elif 1 < t <= 28:
+    elif 1 < t <= 30:
         return theta_pitch
     else:
         return 0
@@ -127,25 +149,41 @@ def theta_pitch_func_numba(t):
 def phi_roll_func_numba(t):
     if 0 <= t <= 1:
         return 0
-    elif 1 < t <= 28:
+    elif 1 < t <= 30:
         return phi_roll
     else:
         return 0
 
-# 仰角
+# ヨー角
+@njit
+def psi_yaw_func_numba(t):
+    if 0 <= t <= 1:
+        return 0
+    elif 1 < t <= 30:
+        return psi_yaw / 29 * (t - 1)   
+    else:
+        return 0
+    
+# 仰角設定
 @njit
 def elev_ang_func_numba(t):
     if 0 <= t <= 1:
         return 0
-    elif 1 < t <= 28:
+    elif 1 < t <= 30:
         return elev_ang
     else:
         return 0
 
-# =======================
-# 機能維持する関数群（Numba対応）
-# =======================
-
+# 方位角設定
+@njit
+def az_center_func_numba(t):
+    if 0 <= t <= 1:
+        return 0
+    elif 1 < t <= 30:
+        return az_center / 29 * (t - 1)   
+    else:
+        return 0
+    
 # 積分
 @njit
 def integ_numba(inp, prev_state, dt):
@@ -155,11 +193,11 @@ def integ_numba(inp, prev_state, dt):
 # 座標変換（機体と地面のxy面の角度差を計算）
 @njit
 def elev_calc_numba(COS, SIN, theta, phi):
-    xe = np.cos(theta * np.pi / 180) * COS + np.sin(phi * np.pi / 180) * np.sin(theta * np.pi / 180) * SIN
-    ye = np.cos(phi * np.pi / 180) * SIN
+    xe =  np.cos(theta * np.pi / 180) * COS + np.sin(phi * np.pi / 180) * np.sin(theta * np.pi / 180) * SIN
+    ye =  np.cos(phi   * np.pi / 180) * SIN
     ze = -np.sin(theta * np.pi / 180) * COS + np.sin(phi * np.pi / 180) * np.cos(theta * np.pi / 180) * SIN
-    r = np.sqrt(max(0,min(xe**2 + ye**2, 1))) # arccos の入力範囲を [-1, 1] に制限
-    clipped_r = max(-1.0, min(r, 1.0))
+    r = np.sqrt(np.maximum(0, np.minimum(xe**2 + ye**2, 1))) # arccos の入力範囲を [-1, 1] に制限
+    clipped_r = np.maximum(-1.0, np.minimum(r, 1.0))
     arccos_r = np.arccos(clipped_r) * 180 / np.pi
     sgn_ze = 1 if ze >= 0 else -1
     el = sgn_ze * arccos_r
@@ -167,13 +205,13 @@ def elev_calc_numba(COS, SIN, theta, phi):
 
 # 角速度リミッター
 @njit
-def v_limiter_numba(inp, v_min=v_min_val, v_max=v_max_val):
-    return max(v_min, min(inp, v_max))
+def v_limiter_numba(inp, v_min, v_max):
+    return np.minimum(np.maximum(inp, v_min),v_max)
 
 # トルクリミッター
 @njit
-def t_limiter_numba(inp, t_min=t_min_val, t_max=t_max_val):
-    return max(t_min, min(inp, t_max))
+def t_limiter_numba(inp, t_min, t_max):
+    return np.minimum(np.maximum(inp, t_min),t_max)
 
 # LPF
 @njit
@@ -197,138 +235,225 @@ def sh_numba(inp, hold_p, i_n, sample_n):
 # =======================
 @njit
 def simulate_numba(
-    steps, dt, azv_d, aza_d, ta, tc, theta_pitch, phi_roll, elev_ang,
-    gretio_el, owntq, JM, DM, JL, DL, kgb, dbrad, kpp, kvp, kvi, v_min_val,
-    v_max_val, t_min_val, t_max_val, fc, sample_n):
+    steps, dt,
+    # azv_d, aza_d, ta, tc, theta_pitch, phi_roll, elev_ang,
+    gretio_el, owntq, JM_el, DM_el, JL_el, DL_el, kgb_el, dbrad_el, kpp_el, kvp_el, kvi_el,
+    v_min_val_el, v_max_val_el, t_min_val_el, t_max_val_el, fc_el,
+    gretio_az,        JM_az, DM_az, JL_az, DL_az, kgb_az, dbrad_az, kpp_az, kvp_az, kvi_az,
+    v_min_val_az, v_max_val_az, t_min_val_az, t_max_val_az, fc_az,
+    sample_n):
     
-    # 初期化
+    # 初期化（共通）
     az_p_deg = 0.0 # AZ角度
     az_p_sh_deg = 0.0 # AZ角度（サンプルホールド後）
-    v_int = 0.0 # 速度積分
-    th1 = 0.0 # モータ角度
-    th1dt1 = 0.0 # モータ角速度
-    th2 = 0.0 # モータ軸換算負荷角度
-    th2dt1 = 0.0 # モータ軸換算負荷角速度
-    y_prev_lpf = 0.0 # LPF
+
+    # 初期化（EL）
+    v_int_el = 0.0 # 速度積分
+    th1_el = 0.0 # モータ角度
+    th1dt1_el = 0.0 # モータ角速度
+    th2_el = 0.0 # モータ軸換算負荷角度
+    th2dt1_el = 0.0 # モータ軸換算負荷角速度
+    y_prev_lpf_el = 0.0 # LPF
+
+    th1_hist_el = np.zeros(steps)
+    th1dt1_hist_el = np.zeros(steps)
+    th2_hist_el = np.zeros(steps)
+    th2dt1_hist_el = np.zeros(steps)
+    tq_hist_el = np.zeros(steps)
+
+    # 初期化（AZ）
+    v_int_az = 0.0 # 速度積分
+    th1_az = 0.0 # モータ角度
+    th1dt1_az = 0.0 # モータ角速度
+    th2_az = 0.0 # モータ軸換算負荷角度
+    th2dt1_az = 0.0 # モータ軸換算負荷角速度
+    y_prev_lpf_az = 0.0 # LPF
+
+    th1_hist_az = np.zeros(steps)
+    th1dt1_hist_az = np.zeros(steps)
+    th2_hist_az = np.zeros(steps)
+    th2dt1_hist_az = np.zeros(steps)
+    tq_hist_az = np.zeros(steps)
     
-    th1_hist = np.zeros(steps)
-    th1dt1_hist = np.zeros(steps)
-    th2_hist = np.zeros(steps)
-    th2dt1_hist = np.zeros(steps)
-    tq_hist = np.zeros(steps)
     az_v_d_hist = np.zeros(steps)
-    az_p_d_hist = np.zeros(steps)
+    az_cmd_d_hist = np.zeros(steps)
     el_cmd_d_hist = np.zeros(steps)
 
     # ループ計算
     for i in range(steps):
         ts = i * dt
         # 連続回転/セクター幅60度/セクター幅180度を選択する。
-        az_v_deg_val = az_v_r_deg_numba(ts) # 連続回転
-        # az_v_deg_val = az_v_s_60deg_numba(ts) # セクター幅60度
+        # az_v_deg_val = az_v_r_deg_numba(ts) # 連続回転
+        az_v_deg_val = az_v_s_60deg_numba(ts) # セクター幅60度
         # az_v_deg_val = az_v_s_180deg_numba(ts) # セクター幅180度
 
         theta_pitch_val = theta_pitch_func_numba(ts) # ピッチ角
         phi_roll_val = phi_roll_func_numba(ts) # ロール角
-        elev_ang_val = elev_ang_func_numba(ts) # ヨー角
+        elev_ang_val = elev_ang_func_numba(ts) # 仰角
 
         az_p_deg = integ_numba(az_v_deg_val, az_p_deg, dt) # AZ角度
-        az_p_sh_deg = sh_numba(az_p_deg, az_p_sh_deg, i, sample_n) # AZ角度
-        az_cmd_deg = az_p_sh_deg + az_center - psi_yaw
+        az_p_sh_deg = sh_numba(az_p_deg, az_p_sh_deg, i, sample_n) # AZ角度(サンプルホールド)
+        az_cmd_deg = az_p_sh_deg + az_center_func_numba(ts) - psi_yaw_func_numba(ts)
         # AZ角度作成 = セクタースキャン角度幅 + 方位角中央値 - ヨー角
+        az_cmd_rad = az_cmd_deg / 180 * np.pi # ragianに変換
+        az_mtr_rad = gretio_az * az_cmd_rad # モータ軸に角度を換算  
         
         cos_az_p = np.cos(az_cmd_deg / 180 * np.pi) # 空中線正面方向ベクトルのX軸成分
         sin_az_p = np.sin(az_cmd_deg / 180 * np.pi) # 空中線正面方向ベクトルのY軸成分
         
         el_calc_deg = elev_calc_numba(cos_az_p, sin_az_p, theta_pitch_val, phi_roll_val)
         el_cmd_deg = el_calc_deg + elev_ang_val # 計算結果 + 仰角
-        el_cmd_rad = el_cmd_deg / 180 * np.pi
-        mtr_rad = gretio_el * el_cmd_rad # モータ軸に換算  
+        el_cmd_rad = el_cmd_deg / 180 * np.pi # ragianに変換
+        el_mtr_rad = gretio_el * el_cmd_rad # モータ軸に角度を換算  
 
-        v_limit = v_limiter_numba(kpp * (mtr_rad - th1)) # 角速度リミッター
-        v_err = v_limit - th1dt1 # 角速度誤差
-        v_int = integ_numba(v_err, v_int, dt) # 角速度誤差の積分
-        tq = t_limiter_numba((kvi * v_int + v_err) * kvp* (JM + JL)) # トルク発生
-        tq = lpf_numba(tq, y_prev_lpf, dt, fc) # LPF
-        tha = np.abs(th2 - th1) # 負荷軸とモータ軸角度の差
-        fn = -np.sign(th2 - th1) * kgb * (tha - dbrad) # ギヤのバネモデルによる反力
+        # ELサーボ
+        v_limit_el = v_limiter_numba(kpp_el * (el_mtr_rad - th1_el), v_min_val_el, v_max_val_el) # 角速度リミッター
+        v_err_el = v_limit_el - th1dt1_el # 角速度誤差
+        v_int_el = integ_numba(v_err_el, v_int_el, dt) # 角速度誤差の積分
+        tqlim_el = t_limiter_numba((kvi_el * v_int_el + v_err_el) * kvp_el * (JM_el + JL_el), t_min_val_el, t_max_val_el)
+        # トルク発生
+        tq_el = lpf_numba(tqlim_el, y_prev_lpf_el, dt, fc_el) # LPF
+        y_prev_lpf_el = tq_el
         
-        if tha < dbrad: # 角度がギヤのバックラッシより小さい時
-            th1dt2 = (tq - DM * th1dt1) / (JM + 1e-9) # モータ軸角加速度
-            th2dt2 = (- DL * th2dt1 - owntq) / (JL + 1e-9) # 負荷軸モータ軸換算加速度
+        tha_el = np.abs(th2_el - th1_el) # 負荷軸とモータ軸角度の差
+        fn_el = -np.sign(th2_el - th1_el) * kgb_el * (tha_el - dbrad_el) # ギヤのバネモデルによる反力
+        
+        if tha_el < dbrad_el: # 角度がギヤのバックラッシより小さい時
+            th1dt2_el = (tq_el - DM_el * th1dt1_el) / (JM_el + 1e-9) # モータ軸角加速度
+            th2dt2_el = (- DL_el * th2dt1_el - owntq) / (JL_el + 1e-9) # 負荷軸モータ軸換算加速度
         else:
-            th1dt2 = (tq - DM * th1dt1 - fn) / (JM + 1e-9)
-            th2dt2 = (fn - DL * th2dt1 - owntq) / (JL + 1e-9)
+            th1dt2_el = (tq_el - DM_el * th1dt1_el - fn_el) / (JM_el + 1e-9)
+            th2dt2_el = (fn_el - DL_el * th2dt1_el - owntq) / (JL_el + 1e-9)
         
-        th1dt1 = integ_numba(th1dt2, th1dt1, dt) # 角加速度を積分して角速度を計算
-        th1  = integ_numba(th1dt1, th1, dt) # 角速度を積分して角度を計算
-        th2dt1 = integ_numba(th2dt2, th2dt1, dt) # 角加速度を積分して角速度を計算
-        th2  = integ_numba(th2dt1, th2, dt) # 角速度を積分して角度を計算
+        th1dt1_el = integ_numba(th1dt2_el, th1dt1_el, dt) # 角加速度を積分して角速度を計算
+        th1_el  = integ_numba(th1dt1_el, th1_el, dt) # 角速度を積分して角度を計算
+        th2dt1_el = integ_numba(th2dt2_el, th2dt1_el, dt) # 角加速度を積分して角速度を計算
+        th2_el  = integ_numba(th2dt1_el, th2_el, dt) # 角速度を積分して角度を計算
+
+        # AZサーボ
+        v_limit_az = v_limiter_numba(kpp_az * (az_mtr_rad - th1_az), v_min_val_az, v_max_val_az) # 角速度リミッター
+        v_err_az = v_limit_az - th1dt1_az # 角速度誤差
+        v_int_az = integ_numba(v_err_az, v_int_az, dt) # 角速度誤差の積分
+        tqlim_az = t_limiter_numba((kvi_az * v_int_az + v_err_az) * kvp_az * (JM_az + JL_az), t_min_val_az, t_max_val_az)
+        # トルク発生
+        tq_az = lpf_numba(tqlim_az, y_prev_lpf_az, dt, fc_az) # LPF
+        y_prev_lpf_az = tq_az
+        
+        tha_az = np.abs(th2_az - th1_az) # 負荷軸とモータ軸角度の差
+        fn_az = -np.sign(th2_az - th1_az) * kgb_az * (tha_az - dbrad_az) # ギヤのバネモデルによる反力
+        
+        if tha_az < dbrad_az: # 角度がギヤのバックラッシより小さい時
+            th1dt2_az = (tq_az - DM_az * th1dt1_az) / (JM_az + 1e-9) # モータ軸角加速度
+            th2dt2_az = (- DL_az * th2dt1_az) / (JL_az + 1e-9) # 負荷軸モータ軸換算加速度
+        else:
+            th1dt2_az = (tq_az - DM_az * th1dt1_az - fn_az) / (JM_az + 1e-9)
+            th2dt2_az = (fn_az - DL_az * th2dt1_az) / (JL_az + 1e-9)
+        
+        th1dt1_az = integ_numba(th1dt2_az, th1dt1_az, dt) # 角加速度を積分して角速度を計算
+        th1_az  = integ_numba(th1dt1_az, th1_az, dt) # 角速度を積分して角度を計算
+        th2dt1_az = integ_numba(th2dt2_az, th2dt1_az, dt) # 角加速度を積分して角速度を計算
+        th2_az  = integ_numba(th2dt1_az, th2_az, dt) # 角速度を積分して角度を計算
 
         # 記録
-        th1_hist[i] = th1
-        th1dt1_hist[i] = th1dt1
-        th2_hist[i] = th2
-        th2dt1_hist[i] = th2dt1
-        tq_hist[i] = tq
+        # AZ/EL共通
         el_cmd_d_hist[i] = el_cmd_deg
         az_v_d_hist[i] = az_v_deg_val
-        az_p_d_hist[i] = az_p_sh_deg
+        az_cmd_d_hist[i] = az_cmd_deg
+        
+        # EL
+        th1_hist_el[i] = th1_el
+        th1dt1_hist_el[i] = th1dt1_el
+        th2_hist_el[i] = th2_el
+        th2dt1_hist_el[i] = th2dt1_el
+        tq_hist_el[i] = tq_el
 
-    return az_v_d_hist, az_p_d_hist, el_cmd_d_hist, tq_hist, th1_hist, th2_hist
+        # AZ
+        th1_hist_az[i] = th1_az
+        th1dt1_hist_az[i] = th1dt1_az
+        th2_hist_az[i] = th2_az
+        th2dt1_hist_az[i] = th2dt1_az
+        tq_hist_az[i] = tq_az
+
+    return az_v_d_hist, az_cmd_d_hist, el_cmd_d_hist, tq_hist_el, th1_hist_el, th2_hist_el, tq_hist_az, th1_hist_az, th2_hist_az
 
 # =======================
 # 実行 & 可視化
 # =======================
-az_v_d_hist, az_p_d_hist, el_cmd_d_hist, tq_hist, th1_hist, th2_hist = simulate_numba(
-    steps, dt, azv_d, aza_d, ta, tc, theta_pitch, phi_roll, elev_ang,
-    gretio_el, owntq, JM, DM, JL, DL, kgb, dbrad, kpp, kvp, kvi, v_min_val,
-    v_max_val, t_min_val, t_max_val, fc, sample_n)
+az_v_d_hist, az_cmd_d_hist, el_cmd_d_hist, tq_hist_el, th1_hist_el, th2_hist_el, tq_hist_az, th1_hist_az, th2_hist_az = simulate_numba(
+    steps, dt,
+    # azv_d, aza_d, ta, tc, theta_pitch, phi_roll, elev_ang,
+    gretio_el, owntq, JM_el, DM_el, JL_el, DL_el, kgb_el, dbrad_el, kpp_el, kvp_el, kvi_el, v_min_val_el,
+    v_max_val_el, t_min_val_el, t_max_val_el, fc_el,
+    gretio_az,        JM_az, DM_az, JL_az, DL_az, kgb_az, dbrad_az, kpp_az, kvp_az, kvi_az, v_min_val_az,
+    v_max_val_az, t_min_val_az, t_max_val_az, fc_az,
+    sample_n)
 
 time = np.linspace(0, sim_time, steps)
 
-fig, axs = plt.subplots(2, 2, figsize=(8, 6)) # 2行2列
+fig, axs = plt.subplots(2, 3, figsize=(10, 6)) # 2行3列
 
-# 左上：el_cmd_deg, th2_deg
+# 左上：el_cmd_deg, el_th2_deg
 axs[0, 0].plot(time, el_cmd_d_hist, label='el_cmd_deg')
-axs[0, 0].plot(time, th2_hist/np.pi*180/gretio_el, label='th2_deg')
-axs[0, 0].set_title('Elevation Angles')
+axs[0, 0].plot(time, th2_hist_el/np.pi*180/gretio_el, label='el_th2_deg')
+axs[0, 0].set_title('EL_Elevation_Angles')
 axs[0, 0].set_xlabel('Time [s]')
 axs[0, 0].set_ylabel('Angle [deg]')
+axs[0, 0].set_xticks(np.arange(0, 31, 5)) # 5秒刻み
 axs[0, 0].legend()
 axs[0, 0].grid(True)
 
-# 右上：delta_deg（y軸設定あり）
-axs[0, 1].plot(time, el_cmd_d_hist - th2_hist/np.pi*180/gretio_el, label='delta_deg')
-axs[0, 1].set_title('delta_deg')
+# 中上：el_delta_deg（y軸設定あり）
+axs[0, 1].plot(time, el_cmd_d_hist - th2_hist_el/np.pi*180/gretio_el, label='EL_delta_deg')
+axs[0, 1].set_title('EL_Delta_Angles')
 axs[0, 1].set_xlabel('Time [s]')
 axs[0, 1].set_ylabel('Angle [deg]')
-axs[0, 1].set_ylim(-1, 1)
-axs[0, 1].set_yticks(np.arange(-1, 1.1, 0.2))  # 0.2度刻み
+axs[0, 1].set_xticks(np.arange(0, 31, 5)) # 5秒刻み
+axs[0, 1].set_yticks(np.arange(-2, 2.1, 0.4)) # 0.2度刻み
 axs[0, 1].legend()
 axs[0, 1].grid(True)
 
-# 左下：az_p_deg
+# 右上：az_delta_deg（y軸設定あり）
+axs[0, 2].plot(time, az_cmd_d_hist - th2_hist_az/np.pi*180/gretio_az, label='AZ_delta_deg')
+axs[0, 2].set_title('AZ_Delta_Angles')
+axs[0, 2].set_xlabel('Time [s]')
+axs[0, 2].set_ylabel('Angle [deg]')
+axs[0, 2].set_xticks(np.arange(0, 31, 5)) # 5秒刻み
+axs[0, 2].set_yticks(np.arange(-2, 2.1, 0.4)) # 0.2度刻み
+axs[0, 2].legend()
+axs[0, 2].grid(True)
+
+# 左下：az_v_deg az_p_deg
 axs[1, 0].plot(time, az_v_d_hist, label='az_v_deg')
-axs[1, 0].plot(time, az_p_d_hist, label='az_p_deg')
-axs[1, 0].set_title('az_v_deg/az_p_deg')
+axs[1, 0].plot(time, az_cmd_d_hist, label='az_cmd_deg')
+axs[1, 0].set_title('az_v_deg/az_cmd_deg')
 axs[1, 0].set_xlabel('Time [s]')
 axs[1, 0].set_ylabel('Angle v [deg/s] p [deg]')
-axs[1, 0].set_yticks(np.arange(-90, 725, 45))  # 0.01度刻み
+axs[1, 0].set_xticks(np.arange(0, 31, 5)) # 5秒刻み
+axs[1, 0].set_yticks(np.arange(-90, 725, 45)) # 45度刻み
 axs[1, 0].legend()
 axs[1, 0].grid(True)
 
-# 右下：tq
-axs[1, 1].plot(time, tq_hist, label='tq')
-axs[1, 1].set_title('tq (Torque)')
+# 中下：el_tq
+axs[1, 1].plot(time, tq_hist_el, label='EL_tq')
+axs[1, 1].set_title('EL_tq (Torque)')
 axs[1, 1].set_xlabel('Time [s]')
-axs[1, 1].set_ylabel('Torque')
-axs[1, 1].set_ylim(-0.04, 0.04)
-axs[1, 1].set_yticks(np.arange(-0.04, 0.045, 0.01))  # 0.01度刻み
+axs[1, 1].set_ylabel('Torque [Nm]')
+axs[1, 1].set_xticks(np.arange(0, 31, 5)) # 5秒刻み
+axs[1, 1].set_yticks(np.arange(-0.04, 0.045, 0.01)) # 0.01度刻み
 axs[1, 1].legend()
 axs[1, 1].grid(True)
 
+# 右下：az_tq
+axs[1, 2].plot(time, tq_hist_az, label='AZ_tq')
+axs[1, 2].set_title('AZ_tq (Torque)')
+axs[1, 2].set_xlabel('Time [s]')
+axs[1, 2].set_ylabel('Torque [Nm]')
+axs[1, 2].set_xticks(np.arange(0, 31, 5)) # 5秒刻み
+axs[1, 2].set_yticks(np.arange(-0.04, 0.045, 0.01)) # 0.01度刻み
+axs[1, 2].legend()
+axs[1, 2].grid(True)
+
 # 全体レイアウト調整と保存
 plt.tight_layout()
-plt.savefig("sim_results.png", dpi=600)
+plt.savefig("sim_out.png", dpi=100)
 plt.show()
